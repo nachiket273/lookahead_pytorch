bf4c5ba7f17d82fb61a1f9572eba87aabd351b21
nachiket273 <nachiket.tanksale@gmail.com>
2019-09-25 19:39:04 +0530

    Implementation of RAdam optimizer(https://arxiv.org/pdf/1908.03265.pdf)

--- RAdam.py
+++ RAdam.py
@@ -1,13 +1,13 @@
 import math
 import torch
-from torch.optim.optimizer import Optimizer
+from torch.optim import Optimizer
 
-class AdamW(Optimizer):
-    r"""Implements Adam algorithm with decoupled weight decay.
 
-    It has been proposed in `DECOUPLED WEIGHT DECAY REGULARIZATION`_.
-    Below is modification of adam code from pytorch repository(https://github.com/pytorch/pytorch)
-
+class RAdam(Optimizer):
+    r"""Implements RAdam algorithm.
+    It has been proposed in `ON THE VARIANCE OF THE ADAPTIVE LEARNING
+    RATE AND BEYOND(https://arxiv.org/pdf/1908.03265.pdf)`_.
+    
     Arguments:
         params (iterable): iterable of parameters to optimize or dicts defining
             parameter groups
@@ -16,15 +16,11 @@ class AdamW(Optimizer):
             running averages of gradient and its square (default: (0.9, 0.999))
         eps (float, optional): term added to the denominator to improve
             numerical stability (default: 1e-8)
-        weight_decay (float, optional): weight decay (default: 0)
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
         amsgrad (boolean, optional): whether to use the AMSGrad variant of this
             algorithm from the paper `On the Convergence of Adam and Beyond`_
             (default: False)
-           
-    .. _DECOUPLED WEIGHT DECAY REGULARIZATION:
-        https://arxiv.org/pdf/1711.05101.pdf
     """
-
     def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
                  weight_decay=0, amsgrad=False):
         if not 0.0 <= lr:
@@ -37,20 +33,14 @@ class AdamW(Optimizer):
             raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
         defaults = dict(lr=lr, betas=betas, eps=eps,
                         weight_decay=weight_decay, amsgrad=amsgrad)
-        super(AdamW, self).__init__(params, defaults)
+        super(RAdam, self).__init__(params, defaults)
 
     def __setstate__(self, state):
-        super(AdamW, self).__setstate__(state)
+        super(RAdam, self).__setstate__(state)
         for group in self.param_groups:
             group.setdefault('amsgrad', False)
 
     def step(self, closure=None):
-        """Performs a single optimization step.
-
-        Arguments:
-            closure (callable, optional): A closure that reevaluates the model
-                and returns the loss.
-        """
         loss = None
         if closure is not None:
             loss = closure()
@@ -59,6 +49,8 @@ class AdamW(Optimizer):
             for p in group['params']:
                 if p.grad is None:
                     continue
+
+                # Perform optimization step
                 grad = p.grad.data
                 if grad.is_sparse:
                     raise RuntimeError('Adam does not support sparse gradients, please consider SparseAdam instead')
@@ -89,22 +81,21 @@ class AdamW(Optimizer):
                 # Decay the first and second moment running average coefficient
                 exp_avg.mul_(beta1).add_(1 - beta1, grad)
                 exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
-                if amsgrad:
-                    # Maintains the maximum of all 2nd moment running avg. till now
-                    torch.max(max_exp_avg_sq, exp_avg_sq, out=max_exp_avg_sq)
-                    # Use the max. for normalizing running avg. of gradient
-                    denom = max_exp_avg_sq.sqrt().add_(group['eps'])
-                else:
-                    denom = exp_avg_sq.sqrt().add_(group['eps'])
 
-                bias_correction1 = 1 - beta1 ** state['step']
-                bias_correction2 = 1 - beta2 ** state['step']
-                
-                step_size = group['lr'] * math.sqrt(bias_correction2) / bias_correction1
-                    
-                p.data.addcdiv_(-step_size, exp_avg, denom)
-                
+                sma_max_len = 2/(1-beta2) - 1  
+                beta2_t = beta2 ** state['step']
+                sma_t = sma_max_len - 2 * state['step'] * beta2_t /(1 - beta2_t)
+
+                if sma_t > 4 :
+                    rt = math.sqrt(((sma_t - 4) * (sma_t - 2) * sma_max_len)/((sma_max_len -4) * (sma_max_len - 2) * sma_t))
+                    denom = exp_avg_sq.sqrt().add_(group['eps'])
+                    step_size = group['lr'] * rt * math.sqrt((1 - beta2_t)) / (1 - beta1 ** state['step'])
+                    p.data.addcdiv_(-step_size, exp_avg, denom)
+                else:
+                    step_size = group['lr'] / (1 - beta1 ** state['step'])
+                    p.data.add_(-step_size, exp_avg)
+
                 if group['weight_decay'] != 0:
                     p.data.add_(-group['weight_decay'] * group['lr'], old)
-                
+
         return loss
--- README.md
+++ README.md
@@ -2,11 +2,31 @@
 
 Pytorch implementation of [Lookahead Optimizer](https://arxiv.org/pdf/1907.08610.pdf)
 
+![Lookahead Algorithm](./fig/lookahead.png)
+
+
+# RAdam Optimizer
+
+[RAdam optimizer](https://arxiv.org/pdf/1908.03265.pdf)
+
+![RAdam Algorithm](./fig/RAdam.png)
+
 # Dependencies
   * PyTorch
 
 # Usage
 
+  Lookahead
+  
+  ```python
   from Lookahead import Lookahead <br/>
   optim = torch.optim.Adam(model.parameters(), lr=0.001 ) <br/>
   optimizer = Lookahead( optim, alpha= 0.6 , k = 10) <br/>
+  ```
+
+  RAdam
+
+  ```python
+  
+
+
